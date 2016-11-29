0cd4a0dc1b93e8740d6d28663cb78e73611b710c
richyg12 <rgazey@gmail.com>
2016-11-29 20:40:47 +0000

    hello form the outside

--- src/test/java/p1/AppTest.java
+++ src/test/java/p1/AppTest.java
@@ -33,7 +33,7 @@ public class AppTest
      */
     public void testApp()
     {
-    	System.out.println("HELLO");
+    	System.out.println("HELLO FROM THE INSIDE");
         assertTrue( true );
     }
 }
